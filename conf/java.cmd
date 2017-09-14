4b0bf9dbe49213977eac219ec44d8c22930be06e
Kervin <zy5211314521@outlook.com>
2017-09-14 09:46:01 +0800

    Linux启动main方法以及依赖包

---
+++ conf/java.cmd
@@ -0,0 +1,5 @@
+Linux启动main方法以及依赖包
+
+$JAVA_HOME/bin/java -Xms2g -Xmx2g -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+AlwaysPreTouch -Xss1m -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Djna.nosys=true -Djdk.io.permissionsUseCanonicalPath=true -Dio.netty.noUnsafe=true -Dio.netty.noKeySetOptimization=true -Dio.netty.recycler.maxCapacityPerThread=0 -Dlog4j.shutdownHookEnabled=false -Dlog4j2.disable.jmx=true -Dlog4j.skipJansi=true -XX:+HeapDumpOnOutOfMemoryError -Delasticsearch -Des.path.home=/home/ELK/elasticsearch-5.5.2 -cp /home/test/ElasticsearchService-1.0.jar:/home/test/lib/* com.share.Main
+
+
