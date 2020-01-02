8d8b8c7abadb114c699f63b8326d12cd16ade3e1
zhangyi <zhangyi183790@hollysys.net>
2020-01-02 14:18:40 +0800

    1.0

---
+++ docker/images/data/hollysys-node-red-v1.0.3/build.sh
@@ -0,0 +1,9 @@
+version='1.0.3'
+name=hollysys-node-red
+docker_name=$name:$version
+docker_register=172.21.32.102:5000
+if [ -n "$(docker images -a|grep -v grep|grep ${docker_register}/$name|grep $version|awk '{print $3}')" ] ; then
+	docker rmi -f $(docker images -a|grep -v grep|grep ${docker_register}/$name|grep $version|awk '{print $3}')
+fi
+docker build --build-arg APP_VERSION=v$version -t ${docker_register}/${docker_name} --rm .
+docker push ${docker_register}/${docker_name}
