79673d679c6fc64229587bc4081db2df0ba5d9dd
张毅183790 <zhangyi183790@hollysys.net>
2020-09-21 14:44:01 +0800

    1.0

--- bash/kafka.sh
+++ bash/kafka.sh
@@ -1,8 +1,11 @@
 rm -rf logs/*
 cd bin
 sh kafka-server-stop.sh
-sleep 5
+sleep 3
 sh kafka-server-start.sh ../config/server.properties > /dev/null 2>&1 &
 pid=$(ps -ef|grep -v grep|grep Kafka|awk '{print $2}')
-sleep 1
+sleep 3 
 echo "--Kafka pid:$pid"
+cat ../logs/server.log
+
+echo "cat logs/server.log"
